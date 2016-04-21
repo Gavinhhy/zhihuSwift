42f56ccad13275dd6b34ed5dc14a4eae1e346f68
Gavin <gavin@gavindemacbook-pro.local>
2016-04-21 14:59:31 +0800

    Initial Commit

---
+++ ZhiHuNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ZhiHuNews
+//
+//  Created by Gavin on 16/4/21.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
