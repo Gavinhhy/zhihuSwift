1a421d13e858a8cf1fc0a819c9062ec46b504c9e
Gavin <gavin@GavindeMacBook-Pro.local>
2016-04-25 14:15:15 +0800

    first commit

---
+++ ZhiHuNews/MyCustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MyCustomTableViewCell.swift
+//  ZhiHuNews
+//
+//  Created by Gavin on 16/4/22.
+//  Copyright © 2016年 Gavin. All rights reserved.
+//
+
+import UIKit
+
+class MyCustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
